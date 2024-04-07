000de492e1651b10cf78977f8624bde8eaa75dc4
Billones142 <stefanomerinoderui@gmail.com>
2024-04-07 17:20:10 -0300

    test

--- Main_test.go
+++ Main_test.go
@@ -2,11 +2,41 @@ package main_test
 
 import (
 	"testing"
+
+	clases "github.dev/Billones142/Prototype/Clases"
 )
 
-func Test_testName(t *testing.T) {
+func Test_ReporteMedico_Clonacion(t *testing.T) {
+	reporteOriginal := clases.Constructor_ReporteMedico(
+		"PDF",
+		"Lucas",
+		"Informe Mensual",
+		"Este es el cuerpo del informe medico",
+		"Dengue",
+	)
 
-	if true {
-		t.Errorf("")
+	copiaReporte := (reporteOriginal.Clonar()).(*clases.ReporteMedico)
+
+	SonIguales := reporteOriginal != copiaReporte
+
+	cuerpo := reporteOriginal.Cuerpo == copiaReporte.Cuerpo
+	formato := reporteOriginal.Formato == copiaReporte.Formato
+	paciente := reporteOriginal.Paciente == copiaReporte.Paciente
+	titulo := reporteOriginal.Titulo == copiaReporte.Titulo
+	sintomas := reporteOriginal.Sintomas == copiaReporte.Sintomas
+	losContenidosNoSonIguales := !(cuerpo && formato && paciente && titulo && sintomas)
+
+	if SonIguales {
+		t.Errorf("No se creo un clon del objeto, es el mismo objeto")
 	}
+
+	if losContenidosNoSonIguales {
+		t.Errorf("los contenidos no son iguales")
+		t.Errorf("cuerpo: %t", cuerpo)
+		t.Errorf("formato: %t", formato)
+		t.Errorf("paciente: %t", paciente)
+		t.Errorf("titulo: %t", titulo)
+		t.Errorf("sintomas: %t", sintomas)
+	}
+
 }
